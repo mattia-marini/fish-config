f42348410646620e95599206aa08331dfc8afcc6
Mattia Marini <marmattia26@gmail.com>
2025-04-17 12:50:25 +0200

    Added gitignore

--- system.fish
+++
@@ -1,7 +0,0 @@
-echo "Loading macos catalina config"
-
-# Set Homebrew environment (Apple Silicon)
-/usr/local/bin/brew shellenv | source
-
-# Latex suite path
-set -gx PATH $PATH /Library/TeX/texbin
